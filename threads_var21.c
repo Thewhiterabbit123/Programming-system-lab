3f0939f545ec99c2e02d20f3bc27c1e9d87f0eb9
workstation <ws@C31.local.bmstu.ru>
2018-04-04 18:55:57 +0400

    opiat ne sdala

--- threads_var21.c
+++ threads_var21.c
@@ -28,7 +28,6 @@ double force_impact(int x, int y, int t) {
       return 1;
     }
     return 0;
-    
 }
 
 int next_time_t(int i, int j, int t) {
@@ -39,15 +38,18 @@ int next_time_t(int i, int j, int t) {
 }
 
 void* solver(void* arg) {
+	printf("start\n");
 	ThreadRecord* data = (ThreadRecord*) arg;
 	for (int k = 1; k < t_grid; ++k){
 		for (int i = data->start; i <  data->end; i++){
 			if (!(i % x_grid == 0 || (i + 1) % x_grid == 0 || i / x_grid == 0 || (i + 1) / x_grid == 0 || i / x_grid == y_grid - 1)){
 				next_time_t(i / x_grid, i % x_grid, k);
-			}	
+			}
+		
 		}
-	pthread_barrier_wait(&barr);
-	pthread_barrier_wait(&barr);
+		printf("t - %d\n", k);
+	//pthread_barrier_wait(&barr);
+	//pthread_barrier_wait(&barr);
 	}
 
 	
@@ -92,15 +94,17 @@ int main(int argc, char const *argv[]) {
 	matrix_memory();
 
 	
-	FILE * gnu = popen("gnuplot -persistent", "w");
+	/*FILE * gnu = popen("gnuplot -persistent", "w");
 	fprintf(gnu, "set terminal gif animate delay 50\n");
 	fprintf(gnu, "set output 'animate.gif'\n");
     
 	fprintf(gnu, "set ticslevel 0\n");
+	
 	fprintf(gnu, "set xrange[0:%d]\n", x_grid-1);
 	fprintf(gnu, "set yrange[0:%d]\n", y_grid-1);
+	fprintf(gnu, "set zrange[-0.2:0.7]\n");
 	  
-	fprintf(gnu, "set dgrid3d\n");
+	fprintf(gnu, "set dgrid3d %d %d \n", x_grid, y_grid);
 	fprintf(gnu, "set hidden3d\n");
 	 
 	fprintf(gnu, "do for [i=1:%d] {\n", t_grid);
@@ -110,10 +114,9 @@ int main(int argc, char const *argv[]) {
 	for (int i = 0; i < x_grid; ++i) {
 	    for (int j = 0; j < y_grid; ++j) {
 	      fprintf(gnu, "%lf %lf %lf\n", (double)i, (double)j, matrix[i][j][0]);
-	      printf("%lf %lf %lf\n", (double)i, (double)j, matrix[i][j][0]);
 	    }
 	}
-	fprintf(gnu, "e\n");
+	fprintf(gnu, "e\n");*/
 	
 	ThreadRecord* threads = NULL;
 	struct timeval tv1, tv2;
@@ -147,18 +150,18 @@ int main(int argc, char const *argv[]) {
 
 
 	
-	for (int k = 1; k < t_grid; ++k) {
+	/*for (int k = 1; k < t_grid; ++k) {
 	pthread_barrier_wait(&barr);
 	  for (int i = 0; i < x_grid; ++i) {
 	    for (int j = 0; j < y_grid; ++j) {
 	      fprintf(gnu, "%lf %lf %lf\n", (double)i, (double)j, matrix[i][j][(k+1)%3]);
-	      printf("%lf %lf %lf\n", (double)i, (double)j, matrix[i][j][(k+1)%3]);
 	    }
 	  }
 	  fprintf(gnu, "e\n");
 	  pthread_barrier_wait(&barr);
 	}
 	pclose(gnu);
+	*/
 	
 	
 	
@@ -168,8 +171,12 @@ int main(int argc, char const *argv[]) {
 
 
    	gettimeofday(&tv2, &tz);
-	printf("%ldms\n", tv2.tv_usec - tv1.tv_usec);
+	printf("%ldms\n", tv2.tv_sec - tv1.tv_sec);
 
+	//x-10000 y-10000 z-100 - 8min, 17sec   1 thread
+	//x-10000 y-10000 z-100 - 4min, 07sec   2 threads
+	//x-10000 y-10000 z-100 - 2min, 05sec   4 threads
+	//x-10000 y-10000 z-100 - 1min, 49 sec  8 threads
 
 
 	return 0;
